ed155c90f88ade715eb4354134258e1f91250fa6
Drew Butler <abutler3@gmail.com>
2014-01-02 14:04:30 -0600

    Add validation to ensure names are specified

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
 feature 'Creating Projects' do
-  scenario "can create a project" do
+  before do
     visit '/'
 
     click_link 'New Project'
+  end
+
+  scenario "can create a project" do
+    # visit '/'
+
+    # click_link 'New Project'
 
     fill_in 'Name', with: 'Lawyer.ly'
     fill_in 'Description', with: 'A social network for lawyers'
@@ -21,4 +27,16 @@ feature 'Creating Projects' do
     expect(page).to have_title(title)
 
   end
+
+  scenario "can not create a project without a name" do
+    # visit '/'
+
+    # click_link 'New Project'
+    click_button 'Create Project'
+
+    expect(page).to have_content('Project has not been created.')
+    expect(page).to have_content("Name can't be blank")
+  end
+
+
 end
