7131e3f798e27a76df985a43da076046ce33093f
Drew Butler <abutler3@gmail.com>
2014-01-28 09:28:53 -0600

    Admin can edit and update users

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,11 +1,12 @@
 FactoryGirl.define do
+  sequence(:email) { |n| "user#{n}@example.com" }
+
   factory :user do
     name "username"
+    email { generate(:email) }
     password "hunter2"
     password_confirmation "hunter2"
     # sequence(:email) { |n| "johndoe#{n}@example.com" }
-    email "sample@example.com"
-
     factory :admin_user do
       admin true
     end
