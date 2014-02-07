2080cadfbad03ba7a02958e11732c0c877956d39
Drew Butler <abutler3@gmail.com>
2014-02-06 20:34:09 -0600

    Restrict creating tickets to only users who have permissions to do it

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,8 +1,8 @@
 class TicketsController < ApplicationController
+  before_action :require_signin!
   before_action :set_project
   before_action :set_ticket, only: [:show, :edit, :update, :destroy]
-  before_action :require_signin!
-
+  before_action :authorize_create!, only: [:new, :create]
   def new
     @ticket = @project.tickets.build
   end
@@ -67,4 +67,11 @@ class TicketsController < ApplicationController
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
+  def authorize_create!
+    if !current_user.admin? && cannot?("create tickets".to_sym, @project)
+      flash[:alert] = "You cannot create tickets on this project."
+      redirect_to @project
+    end
+  end
 end
