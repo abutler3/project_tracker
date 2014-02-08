a9460c7cdb1d98ab92dab69b33eace5105733a86
Drew Butler <abutler3@gmail.com>
2014-02-07 20:34:04 -0600

    Only users with permission can destroy tickets

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -54,6 +54,12 @@ describe TicketsController do
         cannot_update_tickets!
       end
 
+      it "cannot delete a ticket without permission" do
+        delete :destroy, { project_id: project.id, id: ticket.id }
+        # the empty hash is so the params[:ticket] is set
+        expect(response).to redirect_to(project)
+        expect(flash[:alert]).to eql("You cannot delete tickets from this project.")
+      end
     end
   end
 end
